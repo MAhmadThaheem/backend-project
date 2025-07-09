dbc83dd51a3434088924eab3b5cfe3b5d9f2009e
MAhmadThaheem <muhammadahmad28423@gmail.com>
2025-07-10 00:02:22 +0500

    all setup

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,5 @@
 import {asyncHandler} from "../utils/asyncHandler.js";
-import {User} from "../models/user.model.js";
-import {ApiError} from "../utils/ApiError.js";
+
 
 const registerUser = asyncHandler(async (req, res)=>{
     res.status(200).json({message:"ok"})
--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@ import express from "express";
 import { connectDB } from "./db/index.js";
 import dotenv from "dotenv";
 dotenv.config({ path: "./env" });
-const app = express();
-
+import { app } from "./App.js";
 connectDB()
 .then(()=>{
     app.listen(process.env.PORT, () => {

4b4658e958963f08dc2b5747ef1f56134245cbda
MAhmadThaheem <muhammadahmad28423@gmail.com>
2025-07-09 23:49:11 +0500

    routes

--- src/App.js
+++ src/App.js
@@ -14,4 +14,10 @@ app.use(express.urlencoded({extended:true, limit:"16kb"}));//for urls like space
 app.use(express.static("public"));
 app.use(cookieParser());
 
+//routes
+import userRouter from './routes/user.route.js';
+
+//routes declaration
+app.use("/api/v1/users", userRouter);
+
 export {app};
---
+++ src/controllers/user.controller.js
@@ -0,0 +1,8 @@
+import {asyncHandler} from "../utils/asyncHandler.js";
+import {User} from "../models/user.model.js";
+import {ApiError} from "../utils/ApiError.js";
+
+const registerUser = asyncHandler(async (req, res)=>{
+    res.status(200).json({message:"ok"})
+})
+export {registerUser}

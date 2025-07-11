450633ef7e4122347ea2bb8875cee59e10e94e6d
MAhmadThaheem <muhammadahmad28423@gmail.com>
2025-07-11 11:23:55 +0500

    add user channel profile and watch history retrieval features

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -301,6 +301,132 @@ const updateUserCoverImage = asyncHandler(async(req,res)=>{
     )
 })
 
+const getUserChannelProfile = asyncHandler(async(req,res)=>{
+    const {username} = req.params
+    if(!username?.trim()){
+        throw new ApiErorr("Username is required", 400)
+    }
+    const channel = await User.aggregate([
+        {
+            $match: {
+                username: username?.toLowerCase()
+            }
+        },
+        {
+            $lookup:{
+                from:"subscriptions",
+                localField:"_id",
+                foreignField:"channel",
+                as:"subscribers"
+            }
+        },
+        {
+            $lookup: {
+                from:"subscriptions",
+                localField:"_id",
+                foreignField:"subscriber",
+                as:"subscrbedTo"
+            }
+        },
+        {
+            $addFields:{
+                subscriberCount:{
+                    $size:"$subscribers"
+                },
+                subscrbedToCount:{
+                    $size:"$subscrbedTo"
+                },
+                isSubscribed: {
+                    $cond: {
+                        if: {
+                            $in: [req.user?._id, "$subscribers.subscriber"]
+                        },
+                        then: true,
+                        else: false
+                    }
+                }
+            }
+        },
+        {
+            $project:{
+                fullName:1,
+                username:1,
+                email:1,
+                avatar:1,
+                coverImage:1,
+                subscriberCount:1,
+                subscrbedToCount:1,
+                isSubscribed:1
+            }
+        }
+    ])
+    if(!channel?.length){
+        throw new ApiErorr("Channel not found", 404)
+    }
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            channel[0],
+            "Channel profile fetched successfully"
+        )
+    )
+})
+
+const getWatchHistory = asyncHandler(async (req,res) =>{
+    const user = await User.aggregate([
+        {
+            $match:{
+                _id: new mongoose.Types.ObjectId(req.user._id)
+            }
+        },
+        {
+            $lookup:{
+                from: "videos",
+                localField: "watchHistory",
+                foreignField: "_id",
+                as:"watchHistory",
+                pipeline: [
+                    {
+                        $lookup:{
+                            from: "User",
+                            localField: "owner",
+                            foreignField: "_id",
+                            as:"owner",
+                            pipeline: [
+                                {
+                                    $project: {
+                                        fullname:1,
+                                        username:1,
+                                        avatar:1
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    {
+                        $addFields: {
+                            owner: {
+                                $first: "$owner"
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+    ])
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            user[0].watchHistory,
+            "Watch history fetched successfully"
+        )
+    )
+})
+
 export {
     registerUser,
     loginUser,
@@ -310,5 +436,7 @@ export {
     getCurrentUser,
     updateAccountDetails,
     updateUserAvatar,
-    updateUserCoverImage
+    updateUserCoverImage,
+    getUserChannelProfile,
+    getWatchHistory
 }
